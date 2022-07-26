7c6b03662ef99cc6cbfbd7599c25aa2d967640ad
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-07-26 05:56:59 +0000

    added custom SQLAlchemy template

--- src/models.py
+++ src/models.py
@@ -8,23 +8,49 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    ignore = Column(Integer,primary_key=True)
+    user_from_id = Column(Integer,ForeignKey('user.id'))
+    user_to_id = Column(Integer,ForeignKey('user.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250), ForeignKey('person.id'))
+    followers = relationship('Follower')
+    comment = relationship('Comment')
+    post = relationship('Post')
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id')) #Foreign Key of user.id
+    post_id = Column(Integer, ForeignKey('post.id')) #Foreign Key of post.id
+
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer,ForeignKey('user.id')) #Foreign key of user.id
+    comment = relationship('Comment')
+    post = relationship('Media')
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    url = Column(String(250))
+    post_id = Column(Integer,ForeignKey('post.id')) #Foreign key of post.id
+
 
     def to_dict(self):
         return {}
